c87b22d79d10edc17eb9f022489443bc881f941d
Kinich <umansilla@avaya.com>
2019-02-22 13:53:44 -0600

    Primer commit, se integró fron End al proyecto AAADEVEXCRUD.

---
+++ AAADEVEXCRUD/AAADEVEXCRUD-war/src/main/java/service/AAADEVEXCRUD/CustomerBean.java
@@ -0,0 +1,10 @@
+package service.AAADEVEXCRUD;
+
+public class CustomerBean {
+	 	public int accountnum = 0;
+	    public String  email ="";
+	    public String firstname="";
+	    public String lastname="";
+	    public String phone="";
+	    public String preference="";
+}
---
+++ AAADEVEXCRUD/AAADEVEXCRUD-war/src/main/java/service/AAADEVEXCRUD/transactionBean.java
@@ -0,0 +1,11 @@
+package service.AAADEVEXCRUD;
+
+public class transactionBean {
+	
+	public int transid = 0;
+    public String  amount ="";
+    public int accountnum= 0;
+    public String transdate="";
+    public String merchantname="";
+
+}
